acdf7ca09807ac6d8e0446022da2fbc62e9d8289
Chance Robinson <brighamyoung2@gmail.com>
2022-06-16 20:16:24 -0600

    added build

---
+++ react-portfolio/build/precache-manifest.d2e9b4bad95650284b169d48c985b85a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "7615f62575e92ec8d240",
+    "url": "/static/css/main.da3fcad9.chunk.css"
+  },
+  {
+    "revision": "7615f62575e92ec8d240",
+    "url": "/static/js/main.8ecb3545.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c264192e975e8597f3a2",
+    "url": "/static/js/2.040f5720.chunk.js"
+  },
+  {
+    "revision": "6a45bd697ba1f1ba5fb40a86404ce0d3",
+    "url": "/static/media/About_Me_Image.6a45bd69.jpg"
+  },
+  {
+    "revision": "6205720222f1c700a54f13a81535c1b4",
+    "url": "/static/media/Chance-Robinson-Resume-2022.62057202.pdf"
+  },
+  {
+    "revision": "42ab808f92f5b4df358a404cb0043e51",
+    "url": "/static/media/photobomb.42ab808f.PNG"
+  },
+  {
+    "revision": "2d410a17e32b5d0e6ef7eeab6d0987ab",
+    "url": "/static/media/basicWebsite.2d410a17.PNG"
+  },
+  {
+    "revision": "15c6ded8cb4933d09bfb6453d64d9d72",
+    "url": "/static/media/motherEarth.15c6ded8.PNG"
+  },
+  {
+    "revision": "e3f5ffffad2e5b43094d1a8e05aa6093",
+    "url": "/static/media/portfolioReact.e3f5ffff.PNG"
+  },
+  {
+    "revision": "53c1ec5f96d716d4265e536f7576919e",
+    "url": "/static/media/bright-squares.53c1ec5f.png"
+  },
+  {
+    "revision": "60fc89eec1d2ccb86731ffc17614f500",
+    "url": "/index.html"
+  }
+];
